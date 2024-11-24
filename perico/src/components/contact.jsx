5bb3f00ec84067f045d41efa288ec628de47a692
whosmars <mariosalazar@ciencias.unam.mx>
2024-11-24 01:05:03 -0600

    casi final, solo falta la galeria

--- perico/src/components/contact.jsx
+++ perico/src/components/contact.jsx
@@ -93,17 +93,17 @@ export const Contact = (props) => {
                 </div>
                 <div id="success"></div>
                 <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
+                  Enviar Mensaje
                 </button>
               </form>
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
             <div className="contact-item">
-              <h3>Contact Info</h3>
+              <h3>Información de Contacto</h3>
               <p>
                 <span>
-                  <i className="fa fa-map-marker"></i> Address
+                  <i className="fa fa-map-marker"></i> Dirección
                 </span>
                 {props.data ? props.data.address : "loading"}
               </p>
@@ -111,7 +111,7 @@ export const Contact = (props) => {
             <div className="contact-item">
               <p>
                 <span>
-                  <i className="fa fa-phone"></i> Phone
+                  <i className="fa fa-phone"></i> Telefono
                 </span>{" "}
                 {props.data ? props.data.phone : "loading"}
               </p>
@@ -119,7 +119,7 @@ export const Contact = (props) => {
             <div className="contact-item">
               <p>
                 <span>
-                  <i className="fa fa-envelope-o"></i> Email
+                  <i className="fa fa-envelope-o"></i> Correo
                 </span>{" "}
                 {props.data ? props.data.email : "loading"}
               </p>
--- perico/src/components/services.jsx
+++ perico/src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Nuestra Vision</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Nuestro objetivo es mejorar la calidad de vida de los estudiantes a través de innovación, colaboración y prácticas responsables, creando un impacto positivo en la comunidad universitaria.
           </p>
         </div>
         <div className="row">
